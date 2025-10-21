2fee4a496a21b457408ffa1cf6ea74c3c7e210e9
koneke55 <20btrmt034@jainuniversity.ac.in>
2025-10-22 00:54:58 +0530

    Update base path for samboukone.github.io root domain

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/samboukone.github.io">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/project/:projectId" element={<ProjectDetail />} />
